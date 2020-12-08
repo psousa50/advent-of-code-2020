215099d4a761585f112a5167ed5304785b5eff2f
Pedro Sousa <pedronsousa@gmail.com>
2020-12-08 09:41:51 +0000

    Refactor

--- lib/day07/day07.dart
+++ lib/day07/day07.dart
@@ -1,20 +1,20 @@
 import '../common.dart';
 
-class BagContainRule {
-  final int maxCount;
+class BagContainers {
+  final int maxBags;
   final String bagColor;
-  BagContainRule({
-    this.maxCount,
+  BagContainers({
+    this.maxBags,
     this.bagColor,
   });
 }
 
 class Bag {
   final String color;
-  final List<BagContainRule> containRules;
+  final List<BagContainers> containers;
   Bag({
     this.color,
-    this.containRules,
+    this.containers,
   });
 }
 
@@ -29,43 +29,45 @@ class Bags {
 
   Iterable<Bag> thatCanHold(String bagColor) {
     bool canHold(Bag bag) {
-      bool isOrCanHold(BagContainRule rule) {
-        return rule.bagColor == bagColor || canHold(getBag(rule.bagColor));
-      }
-
-      return bag.containRules.where(isOrCanHold).isNotEmpty;
+      return bag.containers
+          .where((rule) =>
+              rule.bagColor == bagColor || canHold(getBag(rule.bagColor)))
+          .isNotEmpty;
     }
 
     return bags.where(canHold);
   }
 
-  int totalBags(String bagColor) {
+  int totalBagsInsideOf(String bagColor) {
     var bag = getBag(bagColor);
-    return bag.containRules.sumBy((b) => b.maxCount) +
-        bag.containRules.map((b) => b.maxCount * totalBags(b.bagColor)).sum();
+    return bag.containers
+        .map((b) => b.maxBags * (1 + totalBagsInsideOf(b.bagColor)))
+        .sum();
   }
 }
 
 Bag parseBag(String line) {
-  var bagAndContainsSplitter = RegExp('([a-z]+ [a-z]+) bags contain(.*)');
-  var bagAndContainsMatches = bagAndContainsSplitter.firstMatch(line);
+  var bagAndContainersSplitter = RegExp('([a-z]+ [a-z]+) bags contain(.*)');
+  var bagAndContainersMatches = bagAndContainersSplitter.firstMatch(line);
 
-  var bagName = bagAndContainsMatches.group(1);
-  var bagContains = bagAndContainsMatches.group(2);
+  var bagName = bagAndContainersMatches.group(1);
+  var bagContainersList = bagAndContainersMatches.group(2);
 
-  var bagContainsSplitter = RegExp('(\s*([0-9])+ ([a-z ]+) bags?)+');
-  var bagContainsMatches = bagContainsSplitter.allMatches(bagContains);
+  var bagContainersSplitter = RegExp('(\s*([0-9])+ ([a-z ]+) bags?)+');
+  var bagContainsMatches = bagContainersSplitter.allMatches(bagContainersList);
 
-  var bagContainsRules = <BagContainRule>[];
+  var bagContainers = bagContainsMatches.fold(
+    <BagContainers>[],
+    (list, m) => list
+      ..add(
+        BagContainers(
+          bagColor: m.group(3),
+          maxBags: int.parse(m.group(2)),
+        ),
+      ),
+  );
 
-  for (var f in bagContainsMatches) {
-    bagContainsRules.add(BagContainRule(
-      bagColor: f.group(3),
-      maxCount: int.parse(f.group(2)),
-    ));
-  }
-
-  return Bag(color: bagName, containRules: bagContainsRules);
+  return Bag(color: bagName, containers: bagContainers);
 }
 
 Bags parseBags(List<String> lines) {
@@ -85,5 +87,5 @@ int day07_part2() {
 
   var bags = parseBags(lines);
 
-  return bags.totalBags('shiny gold');
+  return bags.totalBagsInsideOf('shiny gold');
 }
