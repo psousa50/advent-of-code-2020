87a14962e0c13785680773b742edbb0400ba70f5
Pedro Sousa <pedronsousa@gmail.com>
2020-12-09 23:11:29 +0000

    Day 09 - part 1 - Add recursive version

--- lib/day09/day09.dart
+++ lib/day09/day09.dart
@@ -31,12 +31,31 @@ int incorrectNumber(List<int> numbers, int preamble) {
   return found;
 }
 
+int incorrectNumberRecursive(List<int> numbers, int preamble) {
+  List<List<int>> updatePairsSum(int pos, List<List<int>> pairsSum) {
+    pairsSum = pairsSum.sublist(1)..add([]);
+    for (var p = 0; p < preamble - 1; p++) {
+      pairsSum[p].add(numbers[pos - preamble + p + 1] + numbers[pos]);
+    }
+    return pairsSum;
+  }
+
+  int find(int pos, List<List<int>> pairsSum) {
+    return pairsSum.expand((e) => e).contains(numbers[pos])
+        ? find(pos + 1, updatePairsSum(pos, pairsSum))
+        : numbers[pos];
+  }
+
+  var pairsSum = calculatePairsSum(numbers, preamble);
+  return find(preamble, pairsSum);
+}
+
 int day09_part1() {
   var lines = readLines(9, 'data');
 
   var numbers = lines.map(int.parse).toList();
 
-  return incorrectNumber(numbers, 25);
+  return incorrectNumberRecursive(numbers, 25);
 }
 
 int findNumbersThatSumUpTo(List<int> numbers, int requestedSum) {
